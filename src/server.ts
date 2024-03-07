6e278a26e8140973fdea2cb202866de0884db7f9
Abhishek Aryal <153663851+abhishekaryal1@users.noreply.github.com>
2024-03-07 12:19:50 +0000

    Added Server .ts file

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import { Console } from "console";
+import express from "express";
+
+// We will create an express app
+
+const app = express();
+
+// The port that express server will listen on
+const PORT = 3000;
+
+app.use(express.json());
+
+// Start the express server
+app.listen(PORT, () => {
+    console.log('Server is listening on port ${PORT}');
+});
